57498f15524b4d6519a3f9c612b6f3dba00d7bb4
DK3628 <dk3628@srmist.edu.in>
2026-08-08 15:25:48 +0530

    Add files via upload

---
+++ StudentRecordParser.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class StudentRecordParser
+{
+    void parseStudentRecord(String csvLine)
+    {
+        String fields[] = csvLine.split(",");
+
+        if(fields.length != 3)
+        {
+            System.out.println("Invalid Record");
+        }
+        else
+        {
+            System.out.println("Name: " + fields[0] +
+                               " | Roll No: " + fields[1] +
+                               " | Dept: " + fields[2]);
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter student record: ");
+        String csvLine = sc.nextLine();
+
+        StudentRecordParser obj = new StudentRecordParser();
+        obj.parseStudentRecord(csvLine);
+    }
+}
---
+++ VowelConsonantCounter.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class VowelConsonantCounter
+{
+    void countVowelsAndConsonants(String text)
+    {
+        int vowels = 0;
+        int consonants = 0;
+
+        for(int i = 0; i < text.length(); i++)
+        {
+            char ch = text.charAt(i);
+
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+               ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                vowels++;
+            }
+            else if(ch != ' ')
+            {
+                consonants++;
+            }
+        }
+
+        System.out.println("Total Vowels: " + vowels);
+        System.out.println("Total Consonants: " + consonants);
+    }
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the book title: ");
+        String text = sc.nextLine();
+
+        VowelConsonantCounter obj = new VowelConsonantCounter();
+        obj.countVowelsAndConsonants(text);
+    }
+}
